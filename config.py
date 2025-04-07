5eb38db38e6491750aa80a59583a494c3d2623ba
chaodonghu <dojostan@gmail.com>
2025-04-06 23:23:35 -0400

    edit timing on config

--- config.py
+++ config.py
@@ -1,13 +1,23 @@
 from datetime import datetime
 
 CONFIG = {
-    "artists": ["Drake"],
+    "artists": [
+        "Drake",
+        "Travis Scott",
+        "Kendrick Lamar",
+        "J. Cole",
+        "Nicki Minaj",
+        "Lil Uzi Vert",
+        "Post Malone",
+        "Travis Scott",
+        "Kendrick Lamar",
+        "J. Cole",
+        "Nicki Minaj",
+        "Lil Uzi Vert",
+        "Post Malone",
+        "Giveon",
+    ],
     # Make the playlist name dynamic, based on the current date
     "playlist_id": f"New tracks for the week of {datetime.now().strftime('%m/%d/%Y')}",
-    "days_back": 100,
-    "vibe_filter": {
-        "min_energy": 0.6,
-        "min_danceability": 0.6,
-        "max_acousticness": 0.4,
-    },
-} 
+    "days_back": 7,
+}
